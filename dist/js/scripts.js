fd04cf56608a6347f17df8c87b9b8e80849963b2
kraken1 <pakos1337@gmail.com>
2014-10-29 23:07:42 +0100

    Dodanie nawigacji z wykorzystaniem pluginu sticky

--- dist/js/scripts.js
+++ dist/js/scripts.js
@@ -1,4 +1,12 @@
-/*
+$(window).load(function(){
+  $(".navigation").sticky({ topSpacing: 0, center:true, className:"stickyNav", responsiveWidth:true });
+});
+
+
+//problem jest taki ze:
+//primo - sama strona w ogole chujowo sie laduje :o
+//sekundo - jak ustawilem diva na width 100% to plugin ustawia szerokosc na max! a to jest za duzo bo nie bierze pod uwage
+;/*
 ScrollMagic v1.2.3
 The jQuery plugin for doing magical scroll interactions.
 (c) 2014 Jan Paepke (@janpaepke)
